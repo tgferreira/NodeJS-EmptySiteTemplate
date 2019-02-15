803cd9c93c29699d019b63a5cf2587b3d4c61089
Tiago Ferreira <tiago_moz@hotmail.com>
2019-02-15 15:18:58 -0200

    init

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var ytdl = require('ytdl-core');
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+/* GET: download */
+app.get('/download', function (req, res) {
+
+	let url = req.param('url');
+	let title;
+
+	ytdl(url).on('info', (info) => {
+		title = info.title;
+		ytdl(url).pipe(fs.createWriteStream(title + '.mp3')).on('close', (close) => {
+			console.log('Downloading ' + title);
+			console.log('....Done')
+			var file = title + '.mp3';
+			res.download(file);
+		});
+	});
+});
+
+/* GET: PAGINA HOME */
+app.get('/home', function (req, res) {
+	res.sendfile('./views/home.html');
+});
+
+
+
+/*
+*
+*	SERVER LISTENING
+*
+*/
+app.listen(3000, function () {
+console.log('Listening on port 3000');
+});
+
